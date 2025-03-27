0b2250cd39bfff2285c7b4529c17dd81deb4bee2
amitregev98 <Amit.Regev@e.braude.ac.il>
2025-03-27 14:19:24 +0200

    Amit_update

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+/**
+ * @author Amit_Regev
+ */
 public class SharedData 
 {
 	private ArrayList<Integer> array;
@@ -7,38 +10,60 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * @param array2
+	 * @param b
+	 */
 	public SharedData(ArrayList<Integer> array2, int b) {
 		
 		this.array = array2;
 		this.b = b;
 	}
 
+	/**
+	 * @return
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 	// hi jbgk
 
+	/**
+	 * @param winArray
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
