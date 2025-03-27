e09bcf55bdf86c00be1b9e39c93abcd2b185ab61
amitregev98 <Amit.Regev@e.braude.ac.il>
2025-03-27 15:36:03 +0200

    update2

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 
 /**
- * @author Amit_Regev
+ * Represents shared data accessed by multiple threads for subset checking.
+ * Holds the input array, the target sum, and result flags.
+ * 
+ * @author Amit_Regev 208740092
+ * @version 1.0
  */
 public class SharedData 
 {
@@ -9,63 +13,69 @@ public class SharedData
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	
+
 	/**
-	 * @param array2
-	 * @param b
+	 * Constructs a new SharedData object with the given array and target sum.
+	 * 
+	 * @param array2 the list of integers to check
+	 * @param b the target sum to search for in the subset
 	 */
 	public SharedData(ArrayList<Integer> array2, int b) {
-		
 		this.array = array2;
 		this.b = b;
 	}
 
 	/**
-	 * @return
+	 * Returns the win array indicating which elements form the subset.
+	 * 
+	 * @return the boolean array representing the winning subset
 	 */
-	public boolean[] getWinArray() 
-	{
+	public boolean[] getWinArray() {
 		return winArray;
 	}
-	// hi jbgk
 
 	/**
-	 * @param winArray
+	 * Sets the win array indicating which elements form the subset.
+	 * 
+	 * @param winArray the boolean array to set as the winning subset
 	 */
-	public void setWinArray(boolean [] winArray) 
-	{
+	public void setWinArray(boolean [] winArray) {
 		this.winArray = winArray;
 	}
 
 	/**
-	 * @return
+	 * Returns the input array of integers.
+	 * 
+	 * @return the input array
 	 */
-	public ArrayList<Integer> getArray() 
-	{
+	public ArrayList<Integer> getArray() {
 		return array;
 	}
 
 	/**
-	 * @return
+	 * Returns the target sum to be searched.
+	 * 
+	 * @return the target sum b
 	 */
-	public int getB() 
-	{
+	public int getB() {
 		return b;
 	}
 
 	/**
-	 * @return
+	 * Returns the flag indicating whether a valid subset was found.
+	 * 
+	 * @return true if a subset was found, false otherwise
 	 */
-	public boolean getFlag() 
-	{
+	public boolean getFlag() {
 		return flag;
 	}
 
 	/**
-	 * @param flag
+	 * Sets the flag indicating whether a valid subset was found.
+	 * 
+	 * @param flag true if a subset was found, false otherwise
 	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-
 }
